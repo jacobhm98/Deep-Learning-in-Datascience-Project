b92cf68a1dd8abc2870786822ed90adcb0a417f0
Nick Smyrnioudis <smyrnioudis.nikolaos@outlook.com>
2022-05-05 11:18:02 +0200

    commit for new branch

--- tests/nontests/save_jpg.py
+++ tests/nontests/save_jpg.py
@@ -1,8 +1,13 @@
 import unittest
 
+from data_utils import output_jpg_dir_of_training_data
+
+
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    # Gans comment
+    def test_save_jgp(self):
+        output_jpg_dir_of_training_data("./jpegs/")
+
 
 if __name__ == '__main__':
     unittest.main()
