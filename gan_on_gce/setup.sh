8983ac23f8cb6e5846d8d2bb3c98d02387ce592b
Jacob <jacmalm@kth.se>
2022-05-19 11:45:10 +0200

    training the gan on gce

---
+++ gan_on_gce/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ../..
+mkdir gans_training
+pip install torch==1.7.1+cu110 torchvision==0.8.2+cu110 torchaudio==0.7.2 -f https://download.pytorch.org/whl/torch_stable.html
+git clone https://github.com/NVlabs/stylegan2-ada-pytorch.git
+pip install ninja
+unzip images.zip -d gans_training/
+
